1d759c1018c069c924ce1bdf365d1efd049d2a37
jackc <jackchavers7@gmail.com>
2023-12-15 15:37:41 -0600

    Add project files.

---
+++ JackChavers_HW10_Task1/Program.cs
@@ -0,0 +1,108 @@
+﻿// MIS 3013 HW 10
+
+using a;
+string userChoiceStr;
+
+Console.Write("Do you want to enter a new receipt? (y/n) ");
+userChoiceStr = Console.ReadLine();
+userChoiceStr = userChoiceStr.ToLower();
+
+List<Receipt> receiptList= new List<Receipt>();
+
+while(userChoiceStr=="yes" || userChoiceStr=="y")
+{
+    Console.Write("Please input the number of cogs: ");
+    string nCogsStr = Console.ReadLine();
+    int nCogInt = Convert.ToInt32(nCogsStr);
+
+    Console.Write("Please input the number of gears: ");
+    string nGearsStr = Console.ReadLine();
+    int nGearsInt = Convert.ToInt32(nGearsStr);
+
+    Console.Write("Please input the Customer ID");
+    string customerIDStr = Console.ReadLine();
+    int customerIDInt = Convert.ToInt32(customerIDStr);
+
+    //Receipt r = new Receipt();
+    //r.CustomerID = customerIDInt;
+    //r.CogQuantity = nCogInt;
+    //r.GearQuantity = nGearsInt;
+
+    Receipt r=new Receipt(customerIDInt,nCogInt,nGearsInt);
+
+    r.PrintReciept();
+
+    receiptList.Add(r);
+    Console.Write("Do you want to enter a new receipt? (y/n): ");
+    userChoiceStr = Console.ReadLine();
+    userChoiceStr = userChoiceStr.ToLower();
+}
+
+do
+{
+    Console.WriteLine("Please choose from the options:");
+    Console.WriteLine("1. Print all receipt of one customer");
+    Console.WriteLine("2: Print all receipt for today");
+    Console.WriteLine("3: Print the highest total receipt");
+    Console.WriteLine("4: Print the average grand total");
+    Console.WriteLine("Press other keys to end");
+
+    userChoiceStr = Console.ReadLine();
+
+    if(userChoiceStr=="1")
+    {
+        Console.Write("Please enter a customer id: ");
+        string idStr = Console.ReadLine();
+        int idInt=Convert.ToInt32(idStr);
+        for(int i=0; i<=receiptList.Count; i++)
+        {
+            if (receiptList[i-1].CustomerID==idInt)
+            {
+                receiptList[i-1].PrintReciept();
+            }
+        }
+    }
+    else if(userChoiceStr=="2")
+    {
+        for (int i = 0; i < receiptList.Count; i++)
+        {                     
+                receiptList[i].PrintReciept();
+        }
+    }
+    else if (userChoiceStr == "3")
+    {
+        Receipt highest = receiptList[0];
+        for(int i = 0;i<=receiptList.Count;i++)
+        {
+            if (receiptList[i-1].CalculateTotal()>highest.CalculateTotal())
+            {
+                highest = receiptList[i - 1];
+            }
+        }
+
+        Console.WriteLine("Receipt with highest total:");
+        highest.PrintReciept();
+    }
+    else if (userChoiceStr == "4")
+    {
+        double sumTotal = 0;
+        for(int i=0;i<receiptList.Count;i++)
+        {
+            sumTotal = sumTotal + receiptList[i].CalculateTotal();
+        }
+        double averageTotal=sumTotal/receiptList.Count;
+        Console.WriteLine($"The average of the grand totals is: {averageTotal:C2}");
+    }
+
+
+
+
+
+
+
+}
+while (userChoiceStr=="1" || userChoiceStr=="2" || userChoiceStr=="3" || userChoiceStr =="4");
+
+Console.WriteLine("Thank you and goodbye!");
+
+Console.ReadKey();
---
+++ JackChavers_HW10_Task1/Receipt.cs
@@ -0,0 +1,79 @@
+﻿namespace a
+{
+    public class Receipt
+    {
+        public int CustomerID { get; set; }
+        public int CogQuantity { get; set; }
+        public int GearQuantity { get; set; }
+        public DateTime SalesDate { get; set; }
+        private double SalesTaxPercent { get; set; }
+        private double CogPrice { get; set; }
+        private double GearPrice { get; set; }
+
+        public Receipt()
+        {
+            this.CustomerID = 0;
+            this.CogQuantity = 0;
+            this.GearQuantity = 0;
+
+            this.SalesDate = DateTime.Now;
+            this.SalesTaxPercent = 0.089;
+            this.CogPrice = 79.99;
+            this.GearPrice = 250.00;
+        }
+        public Receipt(int id, int cog, int gear)
+        {
+            CustomerID = id;
+            CogQuantity = cog;
+            GearQuantity = gear;
+
+            this.SalesDate = DateTime.Now;
+            this.SalesTaxPercent = 0.089;
+            this.CogPrice = 79.99;
+            this.GearPrice = 250.00;
+        }
+
+        public double CalculateTotal() 
+        {
+            double netAmount = this.CalculateNetAmount();
+            double salesTaxAmount = this.CalculateTaxAmount();
+            double totalAmount = netAmount + salesTaxAmount;
+            return totalAmount;
+        }
+        public void PrintReciept()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("RECEIPT");
+            Console.WriteLine($"Customer ID: {this.CustomerID}");
+            Console.WriteLine($"# of Cogs: {this.CogQuantity}");
+            Console.WriteLine($"# of Gogs: {this.GearQuantity}");
+            Console.WriteLine($"Net Amount: {this.CalculateNetAmount():C2}");
+            Console.WriteLine($"Sales Tax: {this.CalculateTaxAmount():C2}");
+            Console.WriteLine($"Grand Total: {this.CalculateTotal():C2}");
+            Console.WriteLine($"Time: {this.SalesDate}");
+            Console.WriteLine("==================================");
+        }
+        private double CalculateTaxAmount()
+        {
+            double netAmount = this.CalculateNetAmount();
+            double salesTax = netAmount * this.SalesTaxPercent;
+            return salesTax;
+        }
+        private double CalculateNetAmount() 
+        {
+            double markupPer = 0.15;
+            if (this.CogQuantity > 10 || this.GearQuantity>10 || this.CogQuantity+this.GearQuantity>16)
+            {
+                markupPer = 0.125;
+            }
+            else
+            {
+                markupPer = 0.15;
+            }
+            double netAmount=this.CogQuantity*this.CogPrice*(1+markupPer)+this.GearQuantity*this.GearPrice*(1+markupPer);
+                return netAmount;
+        }
+    }
+
+
+}
